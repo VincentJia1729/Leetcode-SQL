184e981abaac4a9dccd2db74e5c30805550fdd0c
VincentJia1729 <69536655+VincentJia1729@users.noreply.github.com>
2026-01-02 21:07:31 -0500

    Time: 3475 ms (53.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+(SELECT 'Low Salary' as category,
+    SUM (CASE WHEN income < 20000 THEN 1 ELSE 0 END) as accounts_count
+FROM Accounts)
+
+UNION ALL 
+
+(SELECT 'Average Salary' as category,
+    SUM (CASE WHEN income >= 20000 AND income <=50000  THEN 1 ELSE 0 END) as accounts_count
+FROM Accounts)
+
+UNION ALL
+
+(SELECT 'High Salary' as category,
+    SUM (CASE WHEN income > 50000 THEN 1 ELSE 0 END) as accounts_count
+FROM Accounts)
