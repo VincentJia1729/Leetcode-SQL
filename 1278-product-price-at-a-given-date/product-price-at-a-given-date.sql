b484cc2428a8da606dba9ccc39b4365c559a2d17
VincentJia1729 <69536655+VincentJia1729@users.noreply.github.com>
2026-01-02 19:57:11 -0500

    Time: 360 ms (56.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,18 @@
+-- Write your PostgreSQL query statement below
+-- Find the prices of all products on 2019-08-16
+
+(SELECT product_id, 10 as price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16')
+
+UNION ALL
+
+SELECT product_id, new_price as price
+FROM Products
+WHERE (product_id, change_date) IN
+    (SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= DATE '2019-08-16'
+    GROUP BY product_id)
+
